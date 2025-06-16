f70d7ae345441d0ec310e205e1312b5071949142
elisee9 <ketounouelisee9@gmail.com>
2025-06-16 05:23:25 +0100

    modification de la page 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 function NotFound() {
    return (
       <section className="text-center p-10">
-         <h2 className="text-4xl font-bold text-gray-600">
+         <h2 className="text-4xl font-bold text-pink-50 mb-4">
             404 - Page non trouvée
          </h2>
          <p>La page que vous recherchez n'existe pas.</p>
